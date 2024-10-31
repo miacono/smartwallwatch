c29e0c05d0c5588859fd067991fb755a7b19b607
Matteo Iacono <matteo@iacono.io>
2024-10-31 08:34:50 +0100

    fix for pitestenv imgs path

--- pitestenv/start.sh
+++ pitestenv/start.sh
@@ -2,10 +2,10 @@
 
 # This script starts a raspbian emulator
 # 
-# Requires the following files to be in the same directory:
+# Requires the following files to be in the same ./imgs/ directory:
 # 
 # * raspbian.img
 # * kernel-qemu-5.10.63-bullseye
 # * versatile-pb-bullseye-5.10.63.dtb
 
-qemu-system-arm -kernel kernel-qemu-5.10.63-bullseye -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 rootfstype=ext4 rw" -hda raspbian.img -no-reboot -dtb versatile-pb-bullseye-5.10.63.dtb
+qemu-system-arm -kernel ./imgs/kernel-qemu-5.10.63-bullseye -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 rootfstype=ext4 rw" -hda ./imgs/raspbian.img -no-reboot -dtb ./imgs/versatile-pb-bullseye-5.10.63.dtb
